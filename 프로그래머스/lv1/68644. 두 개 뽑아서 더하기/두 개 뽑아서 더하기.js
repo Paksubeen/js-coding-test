d97bf8d625152f68c2a6a15a5c041c7022a6854a
Subeen Park <tnqls520@gmail.com>
2023-02-13 19:57:45 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.47 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -0,0 +1,14 @@
+function solution(numbers) {
+  let answer = [];
+  const set = new Set();
+
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      set.add(numbers[i] + numbers[j]);
+    }
+  }
+
+  answer = [...set].sort((a, b) => a - b);
+
+  return answer;
+}
