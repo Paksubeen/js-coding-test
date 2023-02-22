57d77abdd54dd0522dea59043cf90657a7799eaf
Subeen Park <tnqls520@gmail.com>
2023-02-22 20:06:55 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -0,0 +1,20 @@
+function solution(k, score) {
+  const answer = [];
+  const arr = [];
+
+  for (let i = 0; i < score.length; i++) {
+    if (i < k) {
+      arr.push(score[i]);
+    }
+
+    if (score[i] > Math.min(...arr)) {
+      arr.pop();
+      arr.push(score[i]);
+      arr.sort((a, b) => b - a);
+    }
+
+    answer.push(arr[arr.length - 1]);
+  }
+
+  return answer;
+}
